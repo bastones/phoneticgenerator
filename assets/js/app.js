a7f700ff9d2307e6057d43b0da68ef4f5ffe7eb6
Ben Stones <ben@b3ns.com>
2016-10-25 21:24:35 +0100

    Change animal for letter K.

--- assets/js/app.js
+++ assets/js/app.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
         k: {
             english: 'Kilo',
             deutsch: 'Kaufmann',
-            animals: 'Kitten'
+            animals: 'Koala'
         },
 
         l: {
